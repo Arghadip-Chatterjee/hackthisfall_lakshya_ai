bbe3849f6ced49ea20a8f419a1394c6782107f2e
Arghadip Chatterjee <arghadipchatterjee2016@gmail.com>
2024-11-08 21:25:51 +0530

    2nd Commit

--- src/components/Wordpullupdemo.tsx
+++ src/components/Wordpullupdemo.tsx
@@ -4,7 +4,7 @@ export function WordPullUpDemo() {
   return (
     <WordPullUp
       className="text-4xl font-bold tracking-[-0.02em] text-black dark:text-white md:text-5xl md:leading-[5rem]"
-      words="Made with ❤️ by Team Tech Trio"
+      words="Made with ❤️ by Team Aplha Coders"
     />
   );
 }
